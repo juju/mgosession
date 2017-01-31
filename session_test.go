982e830950f130c0e4cd17dbd67fd81737c1830f
Roger Peppe <rogpeppe@gmail.com>
2017-01-31 09:37:09 +0000

    use custom Logger interface
    This avoids the dependency on the zap package, so packages
    that aren't using that logging framework can use mgosession.

--- session_test.go
+++ session_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	jujutesting "github.com/juju/testing"
-	"golang.org/x/net/context"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/mgosession"
@@ -16,31 +15,35 @@ type suite struct {
 	jujutesting.IsolatedMgoSuite
 }
 
+func (s *suite) TearDownTest(c *gc.C) {
+	s.IsolatedMgoSuite.TearDownTest(c)
+}
+
 var _ = gc.Suite(&suite{})
 
 func (s *suite) TestSession(c *gc.C) {
 	psession := jujutesting.NewProxiedSession(c)
 	defer psession.Close()
-	pool := mgosession.NewPool(context.TODO(), psession.Session, 2)
+	pool := mgosession.NewPool(nil, psession.Session, 2)
 	defer pool.Close()
 
 	// Obtain a session from the pool, then kill its connection
 	// so we can be sure that the next session is using a different
 	// connection
-	s0 := pool.Session(context.TODO())
+	s0 := pool.Session(nil)
 	defer s0.Close()
 	c.Assert(s0.Ping(), gc.IsNil)
 	psession.CloseConns()
 	c.Assert(s0.Ping(), gc.NotNil)
 
 	// The next session should still work.
-	s1 := pool.Session(context.TODO())
+	s1 := pool.Session(nil)
 	defer s1.Close()
 	c.Assert(s1.Ping(), gc.IsNil)
 
 	// The third session should cycle back to the first
 	// and fail.
-	s2 := pool.Session(context.TODO())
+	s2 := pool.Session(nil)
 	defer s2.Close()
 	c.Assert(s2.Ping(), gc.NotNil)
 
@@ -53,17 +56,17 @@ func (s *suite) TestSession(c *gc.C) {
 	// Resetting the pool should cause new sessions
 	// to work again.
 	pool.Reset()
-	s3 := pool.Session(context.TODO())
+	s3 := pool.Session(nil)
 	defer s3.Close()
 	c.Assert(s3.Ping(), gc.IsNil)
-	s4 := pool.Session(context.TODO())
+	s4 := pool.Session(nil)
 	defer s4.Close()
 	c.Assert(s4.Ping(), gc.IsNil)
 }
 
 func (s *suite) TestClosingPoolDoesNotClosePreviousSessions(c *gc.C) {
-	pool := mgosession.NewPool(context.TODO(), s.Session, 2)
-	session := pool.Session(context.TODO())
+	pool := mgosession.NewPool(nil, s.Session, 2)
+	session := pool.Session(nil)
 	defer session.Close()
 	pool.Close()
 	c.Assert(session.Ping(), gc.Equals, nil)
@@ -76,13 +79,13 @@ func (s *suite) TestSessionPinger(c *gc.C) {
 
 	psession := jujutesting.NewProxiedSession(c)
 	defer psession.Close()
-	pool := mgosession.NewPool(context.TODO(), psession.Session, 1)
+	pool := mgosession.NewPool(nil, psession.Session, 1)
 	defer pool.Close()
 
 	// Obtain a session from the pool, then kill its connection
 	// so we tell whether the next session from the pool uses
 	// the same connection.
-	s0 := pool.Session(context.TODO())
+	s0 := pool.Session(nil)
 	defer s0.Close()
 	c.Assert(s0.Ping(), gc.IsNil)
 	psession.CloseConns()
@@ -90,7 +93,7 @@ func (s *suite) TestSessionPinger(c *gc.C) {
 
 	// Sanity check that getting another session
 	// also gives us one that fails.
-	s1 := pool.Session(context.TODO())
+	s1 := pool.Session(nil)
 	defer s1.Close()
 	c.Assert(s0.Ping(), gc.NotNil)
 
@@ -111,7 +114,7 @@ func (s *suite) TestSessionPinger(c *gc.C) {
 	}
 
 	// Now the next session should be all fresh and lovely.
-	s2 := pool.Session(context.TODO())
+	s2 := pool.Session(nil)
 	defer s2.Close()
 	c.Assert(s2.Ping(), gc.IsNil)
 }
